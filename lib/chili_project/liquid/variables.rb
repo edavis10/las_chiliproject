9f3497044ff34d4f487ff23229c6adfec523a629
Eric Davis <edavis@littlestreamsoftware.com>
2011-05-03 13:06:03 -0700

    [#5767] Add messages for the older macros

--- lib/chili_project/liquid/variables.rb
+++ lib/chili_project/liquid/variables.rb
@@ -7,7 +7,9 @@ module ChiliProject
       def self.macro_backwards_compatibility
         {
           'hello_world' => ChiliProject::Liquid::Tags::HelloWorld.new('hello_world','{% hello_world %}','').render(nil),
-          'macro_list' => "Use the '{% variable_list %}' tag to see all Liquid variables and '{% tag_list %}' to see all of the Liquid tags."
+          'macro_list' => "Use the '{% variable_list %}' tag to see all Liquid variables and '{% tag_list %}' to see all of the Liquid tags.",
+          'child_pages' => "Use the '{% child_pages %}' tag",
+          'include' => "Use the '%{ include_page %}' tag"
         }
       end
 
