446a3bd18515bc6b8763e921b2318d9cac80e82a
Eric Davis <edavis@littlestreamsoftware.com>
2011-05-03 12:04:09 -0700

    [#5767] Port the child_pages to Liquid tag

--- app/drops/base_drop.rb
+++ app/drops/base_drop.rb
@@ -2,4 +2,8 @@ class BaseDrop < Liquid::Drop
   def initialize(object)
     @object = object
   end
+
+  def object
+    @object
+  end
 end
--- lib/chili_project/liquid/tags.rb
+++ lib/chili_project/liquid/tags.rb
@@ -2,6 +2,30 @@ module ChiliProject
   module Liquid
     module Tags
 
+      # @param args [Array, String] An array of strings in "key=value" format
+      # @param keys [Hash, Symbol] List of keyword args to extract
+      def self.extract_options(args, *keys)
+        options = {}
+        args.each do |arg|
+          if arg.to_s.gsub(/["']/,'').strip =~ %r{^(.+)\=(.+)$} && keys.include?($1.downcase.to_sym)
+            options[$1.downcase.to_sym] = $2
+          end
+        end
+        return options
+      end
+        
+      class TagError
+        def initialize(tag_name, message)
+          @tag_name = tag_name
+          @message = message
+        end
+
+        def to_s
+          "<div class=\"flash error\">Error executing the <strong>#{@tag_name}</strong> tag (#{@message})</div>"
+        end
+
+      end
+
       # TODO: migration from macros to Liquid
       
       class HelloWorld < ::Liquid::Tag
@@ -46,7 +70,57 @@ module ChiliProject
         end
       end
       
-      # TODO: child_pages
+      class ChildPages < ::Liquid::Tag
+        include ActionView::Helpers::TagHelper
+        include ActionView::Helpers::UrlHelper
+        include ActionView::Helpers::TextHelper
+        include ActionController::UrlWriter
+        include ApplicationHelper
+
+        def self.default_url_options
+          {:only_path => true }
+        end
+
+        def initialize(tag_name, markup, tokens)
+          super
+          if markup.present?
+            tag_args = markup.split(',')
+            @page_name = tag_args.shift.gsub(/["']/,'') # strip quotes
+            @page_name.strip!
+
+            @options = Tags.extract_options(tag_args, :parent)
+            @options[:parent] ||= false
+          end
+        end
+
+        def render(context)
+          if context['project'].present? # inside of a project
+            @project = context['project'].object
+          end
+
+          if @page_name.present? &&
+              (@project.present? || @page_name.include?(':')) # Allow cross project use with project:page_name
+            cross_project_page = @page_name.include?(':')
+
+            page = Wiki.find_page(@page_name.to_s, :project => (cross_project_page ? nil : @project))
+
+            return TagError.new('child_pages', 'Page not found').to_s if page.nil? || !User.current.allowed_to?(:view_wiki_pages, page.wiki.project)
+
+            pages = ([page] + page.descendants).group_by(&:parent_id)
+            return render_page_hierarchy(pages, @options[:parent] ? page.parent_id : page.id)
+          elsif @project.present?
+            @project = context['project'].object
+            return '' unless @project.wiki.present? && @project.wiki.pages.present?
+            return TagError.new('child_pages', 'Page not found').to_s if !User.current.allowed_to?(:view_wiki_pages, @project)
+
+            return render_page_hierarchy(@project.wiki.pages.group_by(&:parent_id))
+          else
+            return TagError.new('child_pages', 'With no argument, this tag can be called from projects only.').to_s
+          end
+          
+        end
+      end
+
       # TODO: include
 
     end
@@ -56,3 +130,4 @@ end
 Liquid::Template.register_tag('hello_world', ChiliProject::Liquid::Tags::HelloWorld)
 Liquid::Template.register_tag('variable_list', ChiliProject::Liquid::Tags::VariableList)
 Liquid::Template.register_tag('tag_list', ChiliProject::Liquid::Tags::TagList)
+Liquid::Template.register_tag('child_pages', ChiliProject::Liquid::Tags::ChildPages)
